4e1bdd194ad25f0f538303f7f6df9513f8a4ec0b
Michael Rodgers <mail@mjrodge.me>
2018-08-17 18:18:01 -0400

    Initial Commit

--- src/header.js
+++ src/header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class Header extends Component {
   render() {
     return (
-      <div id="header">
-        <h1>Hello World</h1>
-      </div>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Title
+          </Typography>
+        </Toolbar>
+      </AppBar>
     );
   }
 }

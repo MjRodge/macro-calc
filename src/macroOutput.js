7737e25f64ece13f6c5c35c327c619e3981961f0
Michael Rodgers <mail@mjrodge.me>
2018-08-22 17:17:33 -0400

    Added Data Visualisation
    Added react-chartjs-2 package to easily add chart-js components to provide visualisation of calculated macro and calorie goals.

--- src/macroOutput.js
+++ src/macroOutput.js
@@ -1,17 +1,71 @@
 import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
+import { Doughnut, HorizontalBar } from 'react-chartjs-2';
 
 class MacroOutput extends Component {
   render() {
+    var macroData = {
+      labels: ["Protein", "Fat", "Carbohydrate"],
+      datasets: [
+      {
+        label: 'Macronutrients Allowance',
+        data: [this.props.passedProtein, this.props.passedFat, this.props.passedCarbs],
+        backgroundColor: [
+          '#36A2EB',
+          '#FFCE56',
+          '#FF6384',
+        ]
+      }]
+    };
+
+    var calorieData = {
+        labels: ["cals"],
+
+        datasets: [{
+          label: 'Resting Calorie Expenditure',
+          data: [this.props.passedRCE],
+          backgroundColor: '#36A2EB',
+        },
+        {
+          label: 'Total Calorie Expenditure',
+          data: [this.props.passedTotalCals],
+          backgroundColor: '#FFCE56',
+        },
+        {
+          label: 'Goal Calorie Intake',
+          data: [this.props.passedGoalCals],
+          backgroundColor: '#FF6384',
+        }]
+    };
+    var calorieOptions = {
+      maintainAspectRatio: false,
+      responsive: true,
+      scales: {
+        xAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }],
+      }
+    }
+
     return (
-      <Paper elevation={1} className="input-paper">
-        <h2>RESTING CALS: {this.props.passedRCE}</h2>
-        <h2>TOTAL CALS: {this.props.passedTotalCals}</h2>
-        <h2>GOAL CALS: {this.props.passedGoalCals}</h2>
-        <h2>Total protein: {this.props.passedProtein}g</h2>
-        <h2>Total fat: {this.props.passedFat}g</h2>
-        <h2>Total carbs: {this.props.passedCarbs}g</h2>
-      </Paper>
+      <div>
+        <Paper elevation={1} className="input-paper macro-output">
+          <Doughnut data={macroData} options={{maintainAspectRatio: false, responsive: true}}/>
+        </Paper>
+        <Paper elevation={1} className="input-paper macro-output">
+          <HorizontalBar data={calorieData} options={calorieOptions}/>
+        </Paper>
+        <Paper elevation={1} className="input-paper">
+          <h2>RESTING CALS: {this.props.passedRCE}</h2>
+          <h2>TOTAL CALS: {this.props.passedTotalCals}</h2>
+          <h2>GOAL CALS: {this.props.passedGoalCals}</h2>
+          <h2>Total protein: {this.props.passedProtein}g</h2>
+          <h2>Total fat: {this.props.passedFat}g</h2>
+          <h2>Total carbs: {this.props.passedCarbs}g</h2>
+        </Paper>
+      </div>
     );
   }
 }

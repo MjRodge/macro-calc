2f3f3192758c8d94dedf769951297c7586c78ba1
Michael Rodgers <mail@mjrodge.me>
2018-09-03 17:35:54 -0400

    Unit Menu
    Decided to use radio boxes for unit selection.
    Altered textfield drop down to use radio instead

--- src/header.js
+++ src/header.js
@@ -6,23 +6,13 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-
 import Divider from '@material-ui/core/Divider';
 import './css/header.css';
-
-const style = {
-  textField: {
-    width: '87%',
-  },
-}
 
 class Header extends Component {
   constructor(props) {
@@ -40,11 +30,11 @@ class Header extends Component {
     });
   };
 
-  handleWeightUnit = weightUnit => event => {
-      this.setState({ [weightUnit]: event.target.value });
+  handleWeightUnit = event => {
+      this.setState({ weightUnit: event.target.value });
       this.props.passedWeightUnit(event.target.value);
     };
-  handleChange = event => {
+  handleHeightUnit = event => {
     this.setState({ heightUnit: event.target.value });
     this.props.passedHeightUnit(event.target.value);
   };
@@ -73,39 +63,39 @@ class Header extends Component {
                     </IconButton>
                   </Toolbar>
                 </AppBar>
-                <div className="unit-options">
-                  <TextField
-                    style={style.textField}
-                    select
-                    label="Weight Unit"
-                    value={this.state.weightUnit}
-                    onChange={this.handleWeightUnit("weightUnit")}
-                    margin="normal"
-                  >
-                    <MenuItem value="kg">
-                      Kilograms
-                    </MenuItem>
-                    <MenuItem value="lb">
-                      Pounds
-                    </MenuItem>
-                  </TextField>
-                </div>
-                <Divider />
-                <div className="unit-options">
-                  <FormControl component="fieldset">
-                    <FormLabel component="legend">Height Unit</FormLabel>
+                <div className="menu-content">
+                  <Divider />
+                  <div className="unit-options">
+                    <FormControl component="fieldset">
+                      <FormLabel component="legend">Weight Unit</FormLabel>
                       <RadioGroup
-                        aria-label="Gender"
-                        name="gender1"
-                        value={this.state.heightUnit}
-                        onChange={this.handleChange}
+                        aria-label="Weight"
+                        name="weight"
+                        value={this.state.weightUnit}
+                        onChange={this.handleWeightUnit}
                       >
-                        <FormControlLabel value="cm" control={<Radio />} label="Centimetres" />
-                        <FormControlLabel value="ft/in" control={<Radio />} label="Feet & Inches" />
+                        <FormControlLabel value="kg" control={<Radio />} label="Kilograms" />
+                        <FormControlLabel value="lb" control={<Radio />} label="Pounds" />
                       </RadioGroup>
-                  </FormControl>
+                    </FormControl>
+                  </div>
+                  <Divider />
+                  <div className="unit-options">
+                    <FormControl component="fieldset">
+                      <FormLabel component="legend">Height Unit</FormLabel>
+                        <RadioGroup
+                          aria-label="Height"
+                          name="height"
+                          value={this.state.heightUnit}
+                          onChange={this.handleHeightUnit}
+                        >
+                          <FormControlLabel value="cm" control={<Radio />} label="Centimetres" />
+                          <FormControlLabel value="ft/in" control={<Radio />} label="Feet & Inches" />
+                        </RadioGroup>
+                      </FormControl>
+                    </div>
+                  <Divider />
                 </div>
-                <Divider />
               </div>
             </SwipeableDrawer>
           </IconButton>
